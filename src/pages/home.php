a18b1845d6c89fc1e85e575e9ca87a70b801dd29
Emir Zakrewski <frenkchambers@gmail.com>
2023-05-06 14:41:58 +0300

    html: home правки

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,209 +8,224 @@
     <link rel="stylesheet" href="styles/sass/">
     <title>Колледж архитектуры и строительства</title>
 </head>
-<body>
-   <!-- Start header -->
-	<header class="header">
-		<div class="wrapper">
-			<h2>МИНИСТЕРСТВО ОБРАЗОВАНИЯ ПЕНЗЕНСКОЙ ОБЛАСТИ <br>
-                ГОСУДАРСТВЕННОЕ АВТОНОМНОЕ ПРОФЕССИОНАЛЬНОЕ <br>
-                ОБРАЗОВАТЕЛЬНОЕ УЧРЕЖДЕНИЕ ПЕНЗЕНСКОЙ ОБЛАСТИ</h2>
-                <div class="intro">
-                    <h1>ПКАС</h1>
-                    <div class="header-logo">
-                        <img src="" alt="">
-         </div>
-                    <div class="under-intro">
-                        <h2>Пензенский колледж архитектуры <br>
-                        и строительства
-                        </h2>
-                    </div>
-                </div>
-		</div>
-	</header>
-	<!-- End header -->
 
-	<!-- Start navigation -->
-	<div class="nav-area">
-		<div class="wrapper">
-            <div class="nav">
-                <ul class="row-area">
-                    <span class="row">Навигация</span>
-                </ul>
-                <ul class="row-area">
-                    <span class="row">Для ученика</span>
-                    <li><a href="news.php">Новости</a></li>
-                    <li><a href="#">Контакты</a></li>
-                    <li><a href="apply_for_education.php">Подать заявление</a></li>
-                    <li><a href="professions.php">Навигатор профессий</a></li>
-                    <li><a href="dormitory.php">Общежитие</a></li>
-                    <li><a href="timetable.php">Расписание</a></li>
-                    <li><a href="history.php">История колледжа</a></li>
-                    <li><a href="documents.php">Документы</a></li>
-                    <li><a href="actions.php">Мероприятия</a></li>
-                    <li><a href="psychologist.php">Страница психолога</a></li>
-                    <li><a href="workshops.php">Мастерские</a></li>
-                </ul>
-                <ul class="row-area">
-                    <span class="row">Для преподавателя</span>
-                    <li><a href="certification.php">Аттестация педагогических
-                        работников</a></li>
-                    <li><a href="scientific_methodological_support.php">Научно-методическое
-                        обеспечение
-                        образовательного
-                        процесса</a></li>
-                </ul>
+<body>
+    <!-- Start header -->
+    <header class="header">
+        <div class="wrapper">
+            <span class="big-text">МИНИСТЕРСТВО ОБРАЗОВАНИЯ ПЕНЗЕНСКОЙ ОБЛАСТИ <br>
+                ГОСУДАРСТВЕННОЕ АВТОНОМНОЕ ПРОФЕССИОНАЛЬНОЕ <br>
+                ОБРАЗОВАТЕЛЬНОЕ УЧРЕЖДЕНИЕ ПЕНЗЕНСКОЙ ОБЛАСТИ</span>
+            <div class="intro">
+                <h1>ПКАС</h1>
+                <div class="header-logo">
+                    <img src="" alt="">
+                </div>
+                <div class="under-intro">
+                    <h2>Пензенский колледж архитектуры <br>
+                        и строительства
+                    </h2>
+                </div>
             </div>
         </div>
-	</div>
-	<!-- End navigation -->
+    </header>
+    <!-- End header -->
 
-	<!-- Start tel menu -->
+    <!-- Start navigation -->
+    <div class="nav-area">
+        <div class="wrapper">
+            <nav>
+                <ul>
+                    <div>
+                        <span class="row">Навигация</span>
+                    </div>
+                    <div>
+                        <span class="row">Для ученика</span>
+                        <ul class="row-area">
+                            <li><a href="news.php">Новости</a></li>
+                            <li><a href="#">Контакты</a></li>
+                            <li><a href="apply_for_education.php">Подать заявление</a></li>
+                            <li><a href="professions.php">Навигатор профессий</a></li>
+                            <li><a href="dormitory.php">Общежитие</a></li>
+                            <li><a href="timetable.php">Расписание</a></li>
+                            <li><a href="history.php">История колледжа</a></li>
+                            <li><a href="documents.php">Документы</a></li>
+                            <li><a href="actions.php">Мероприятия</a></li>
+                            <li><a href="psychologist.php">Страница психолога</a></li>
+                            <li><a href="workshops.php">Мастерские</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <span class="row">Для преподавателя</span>
+                        <ul class="row-area">
+                            <li><a href="certification.php">Аттестация педагогических
+                                    работников</a></li>
+                            <li><a href="scientific_methodological_support.php">Научно-методическое
+                                    обеспечение
+                                    образовательного
+                                    процесса</a></li>
+                        </ul>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <!-- End navigation -->
+
+    <!-- Start tel menu -->
     <div class="tel-menu-area">
         <div class="wrapper">
             <div class="tel-menu">
                 <div class="main-text">
                     <p>
-                    Телефоны "горячей линии" в колледже, для <br>
-                    консультирования клиентов, оформления заявок, <br>
-                    оказания технической поддержки:
+                        Телефоны "горячей линии" в колледже, для <br>
+                        консультирования клиентов, оформления заявок, <br>
+                        оказания технической поддержки:
                     </p>
                 </div>
                 <div class="tel-n-hotline">
                     <div class="tel">
-                    <p>
-                        8 (908)530-38-08; <br>
-                        8 (412) 52-18-58, <br>
-                        8 (412) 43-44-86. <br>
-                    </p>
+                        <p>
+                            8 (908)530-38-08; <br>
+                            8 (412) 52-18-58, <br>
+                            8 (412) 43-44-86. <br>
+                        </p>
                     </div>
                     <div class="hotline">
-                    Время работы "горячей <br>
-                    линии": рабочие дни с <br>
-                    9.00 до 12.00, с 13.00 до <br>
-                    16.00
+                        Время работы "горячей <br>
+                        линии": рабочие дни с <br>
+                        9.00 до 12.00, с 13.00 до <br>
+                        16.00
                     </div>
                 </div>
             </div>
-        <div class="btn-tel">
-    <a href="#">Звоните <br>
-                прямо <br> 
-                сейчас</a>
-             </div>
-        </div>
-    </div>
-    
-	<!-- End tel menu -->
-
-	<!-- Start main news -->
-    <div class="main-news-area">
-        <div class="wrapper">
-            <div class="main-news">
-                <h2>ГЛАВНАЯ <br>
-                    НОВОСТИ
-                </h2>
-                <p>Пензенского колледжа архитектуры и <br>
-                строительства</p>
-                <div class="news">
-                    <div class="img-news">
-                        <img src="../static/img/news/news-test.png" alt="">
-                        <img src="../static/img/news/news-test2.png" alt="">
-                    </div>
-                    <div class="text-news">
-                        <p>ДИКТАНТ ПОБЕДЫ</p><br>
-                        <p>27 апреля 2023</p><br>
-                        <p>В Пензенском колледже архитектуры и <br>
-                        строительства 27 апреля студенты  <br>
-                        написали Диктант Победы.</p><br>
-                    </div>
-                    <div class="btn-news">
-                        <img src="../static/img/news/arrow.png" alt="">
-                        <p>читать</p>
-                    </div>
-                </div>
-                <a href="news.php" class="all-news">ЧИТАТЬ ВСЕ НОВОСТИ</a>
+            <div class="btn-tel">
+                <a href="#">Звоните <br>
+                    прямо <br>
+                    сейчас</a>
             </div>
         </div>
     </div>
-	<!-- End main news -->
 
-	<!-- Start footer -->
-	<div class="footer-area">
+    <!-- End tel menu -->
+
+    <!-- Start main news -->
+    <main>
+        <div class="main-news-area">
+            <div class="wrapper">
+                <div class="main-news">
+                    <h2>ГЛАВНАЯ <br>
+                        НОВОСТИ
+                    </h2>
+                    <p>Пензенского колледжа архитектуры и <br>
+                        строительства</p>
+                    <div class="news">
+                        <div class="img-news">
+                            <img src="../static/img/news/news-test.png" alt="">
+                            <img src="../static/img/news/news-test2.png" alt="">
+                        </div>
+                        <div class="text-news">
+                            <p>ДИКТАНТ ПОБЕДЫ</p><br>
+                            <p>27 апреля 2023</p><br>
+                            <p>В Пензенском колледже архитектуры и <br>
+                                строительства 27 апреля студенты <br>
+                                написали Диктант Победы.</p><br>
+                        </div>
+                        <div class="btn-news">
+                            <img src="../static/img/news/arrow.png" alt="">
+                            <p>читать</p>
+                        </div>
+                    </div>
+                    <a href="news.php" class="all-news">ЧИТАТЬ ВСЕ НОВОСТИ</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- End main news -->
+
+    <!-- Start footer -->
+    <footer class="footer-area">
         <div class="wrapper">
             <div class="footer">
-                <ul class="column-area">
+                <div>
                     <span class="row">ВАЖНЫЕ НОМЕРА</span>
-                    <li>Телефонная линия "Ребенок в <br>
-                    опасности" следственного управления <br>
-                    Следственного комитета России по <br> 
-                    Пензенской области 36-17-19 с <br>
-                    мобильных телефонов 123</li>
-                    <li>"Телефон доверия" следственного <br>
-                    управления Следственного комитета <br>
-                    России по Пензенской области <br>
-                    36-17-58</li>
-                    <li>"Телефон горячей линии По вопросам <br>
-                    обновления содержания общего <br>
-                    образования на сайтах <br>
-                    образовательных организаций в сети <br>
-                    "Интернет" 8-800-200-91-85, <br>
-                    добавочный 7</li>
-                </ul>
+                    <ul class="column-area">
+                        <li>Телефонная линия "Ребенок в <br>
+                            опасности" следственного управления <br>
+                            Следственного комитета России по <br>
+                            Пензенской области 36-17-19 с <br>
+                            мобильных телефонов 123</li>
+                        <li>"Телефон доверия" следственного <br>
+                            управления Следственного комитета <br>
+                            России по Пензенской области <br>
+                            36-17-58</li>
+                        <li>"Телефон горячей линии По вопросам <br>
+                            обновления содержания общего <br>
+                            образования на сайтах <br>
+                            образовательных организаций в сети <br>
+                            "Интернет" 8-800-200-91-85, <br>
+                            добавочный 7</li>
+                    </ul>
+                </div>
                 <ul class="column-area">
                     <span class="row">КОНТАКТЫ</span>
                     <li>г. Пенза, ул. Набережная р. Пензы, д. 3
-                    Администрация: <a href="http://pkas58.ru/article/sitemap">pask_e@mail.ru</a><br> 
-                    <a href="http://pkas58.ru/article/sitemap ">Карта портала</a> <br>
-                    <a href="http://vk.com/pkas_58">Официальная группа вконтакте</a> <br>
-                    <a href="http://pkas58.ru/wp/?page_id=77"> Форма обратной связи</a></li>
+                        Администрация: <a href="http://pkas58.ru/article/sitemap">pask_e@mail.ru</a><br>
+                        <a href="http://pkas58.ru/article/sitemap ">Карта портала</a> <br>
+                        <a href="http://vk.com/pkas_58">Официальная группа вконтакте</a> <br>
+                        <a href="http://pkas58.ru/wp/?page_id=77"> Форма обратной связи</a>
+                    </li>
                     <li>Вопросы по работе сайта: +7(900)316-04-47 <br>
-                    Надежда Юрьевна Щербакова</li>
+                        Надежда Юрьевна Щербакова</li>
                 </ul>
                 <ul class="column-area">
                     <span class="row">ОБРАТНАЯ СВЯЗЬ</span>
                     <li>Есть предложения по организации <br>
-                    учебного процесса или знаете, как<br>
-                    сделать колледж лучше? </li>
+                        учебного процесса или знаете, как<br>
+                        сделать колледж лучше? </li>
                     <li><button class="btn-complaint" href="#">Написать о жалобе</button></li>
                 </ul>
             </div>
         </div>
-	</div>
-	<!-- End footer -->
+    </footer>
+    <!-- End footer -->
 
-    <!-- Start Modal fidback -->
-        <div class="modal-fid">
-            <div class="modal">
-            <div class="form-group">
-                <label for="login">Ваше ФИО<span class="">*</span></label>
-                <input type="text"  id="username" class="form-control" type="">
-             </div>
-             <div class="form-group">
-                <label for="login">Почта<span class="">*</span></label>
-                <input type="text"  id="email" class="form-control" type="email">
-             </div>
-             <div class="form-group">
-                <label for="">Опишите ситуацию<span class="">*</span></label>
-                <textarea class="form-control" name="con_message" placeholder="Сообщение"></textarea>
-             </div class="form-group">
-             <p>
-                Если у Вас несколько вопросов, напишите отдельное сообщение <br>
-                по каждому  
-             </p>
-             <div class="form-group">
-                <input type="checkbox" id="" name="">
-                <label for="">Сообщение от имени юридического лица</label>
-            </div>
-            <div class="form-group">
-                <input type="checkbox" id="" name="">
-                <label for="">Я соглашаюсь с правилами подачи сообщения</label>
-            </div>
-            <div class="btn-modal">
-                <button class="btn-complaint">Добавить файл</button>
-                <button class="btn-complaint">Отправить</button>
-            </div>
-            </div>
+    <!-- Start Modal feedback -->
+    <div class="modal-fid">
+        <div class="modal">
+            <form>
+                <div class="form-group">
+                    <label for="login">Ваше ФИО<span class="">*</span></label>
+                    <input type="text" id="username" class="form-control" type="">
+                </div>
+                <div class="form-group">
+                    <label for="login">Почта<span class="">*</span></label>
+                    <input type="text" id="email" class="form-control" type="email">
+                </div>
+                <div class="form-group">
+                    <label for="">Опишите ситуацию<span class="">*</span></label>
+                    <textarea class="form-control" name="con_message" placeholder="Сообщение"></textarea>
+                </div class="form-group">
+                <p>
+                    Если у Вас несколько вопросов, напишите отдельное сообщение <br>
+                    по каждому
+                </p>
+                <div class="form-group">
+                    <input type="checkbox" id="" name="">
+                    <label for="">Сообщение от имени юридического лица</label>
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" id="" name="">
+                    <label for="">Я соглашаюсь с правилами подачи сообщения</label>
+                </div>
+                <div class="btn-modal">
+                    <button class="btn-complaint">Добавить файл</button>
+                    <button class="btn-complaint">Отправить</button>
+                </div>
+            </form>
         </div>
+    </div>
     <!-- End Modal fidback-->
 </body>
+
 </html>
